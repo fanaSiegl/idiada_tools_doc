537c092318e43eeac888961ccc503786054bd3b5
Frantisek Siegl <frantisek.siegl@idiada.cz>
2020-03-19 17:57:17 +0100

    Functional files removed.

--- doc/sphinx/buildHtmlDoc.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-import os
-
-os.system('/data/fem/envs/python35_pyqt4/bin/sphinx-build -b html -d build/doctrees source build/html')
-
-print("Build finished.")
